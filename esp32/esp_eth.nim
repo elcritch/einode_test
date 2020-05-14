99cfd74fa989cebc4452ebaefecd2df2a5904539
Jaremy Creechley <creechley@gmail.com>
2020-05-14 01:18:05 -0600

    fixing up esp32 eth items

--- esp32/esp_eth.nim
+++ esp32/esp_eth.nim
@@ -11,7 +11,7 @@
 ##  See the License for the specific language governing permissions and
 ##  limitations under the License.
 
-import err
+import esp_err
 
 ## *
 ##  @brief Ethernet interface mode
--- esp32/eth.nim
+++ esp32/eth.nim
@@ -22,44 +22,58 @@ import arduino
 import esp_eth
 
 type
-  eth_phy_type_t* = enum
+  eth_phy_type_t* {.size: sizeof(cint), importcpp: "eth_phy_type_t", header: "ETH.h".} = enum
     ETH_PHY_LAN8720, ETH_PHY_TLK110, ETH_PHY_MAX
 
-const 
-  ETH_PHY_ADDR = 0
-  ETH_PHY_TYPE = ETH_PHY_LAN8720
-  ETH_PHY_POWER = -1
-  ETH_PHY_MDC = 23
-  ETH_PHY_MDIO = 18
-  ETH_CLK_MODE = 0
 
 type
-  ETHClass* {.bycopy.} = object
+  ETHClass* {.importcpp: "ETHClass", header: "ETH.h", bycopy.} = object
 
 
-proc constructETHClass*(): ETHClass {.cdecl, constructor.}
-proc destroyETHClass*(this: var ETHClass) {.cdecl.}
+proc constructETHClass*(): ETHClass {.cdecl, constructor, importcpp: "ETHClass(@)",
+                                   header: "ETH.h".}
+proc destroyETHClass*(this: var ETHClass) {.cdecl, importcpp: "#.~ETHClass()",
+                                        header: "ETH.h".}
+
 proc begin*(this: var ETHClass; phy_addr: uint8 = ETH_PHY_ADDR;
            power: cint = ETH_PHY_POWER; mdc: cint = ETH_PHY_MDC;
            mdio: cint = ETH_PHY_MDIO; `type`: eth_phy_type_t = ETH_PHY_TYPE;
-           clk_mode: eth_clock_mode_t = ETH_CLOCK_GPIO0_IN): bool {.cdecl.}
+           clk_mode: eth_clock_mode_t = ETH_CLK_MODE): bool {.cdecl,
+    importcpp: "begin", header: "ETH.h".}
 proc config*(this: var ETHClass; local_ip: IPAddress; gateway: IPAddress;
             subnet: IPAddress; dns1: IPAddress = cast[uint32](0x00000000);
-            dns2: IPAddress = cast[uint32](0x00000000)): bool {.cdecl.}
-proc getHostname*(this: var ETHClass): cstring {.cdecl.}
-proc setHostname*(this: var ETHClass; hostname: cstring): bool {.cdecl.}
-proc fullDuplex*(this: var ETHClass): bool {.cdecl.}
-proc linkUp*(this: var ETHClass): bool {.cdecl.}
-proc linkSpeed*(this: var ETHClass): uint8 {.cdecl.}
-proc enableIpV6*(this: var ETHClass): bool {.cdecl.}
-proc localIPv6*(this: var ETHClass): IPv6Address {.cdecl.}
-proc localIP*(this: var ETHClass): IPAddress {.cdecl.}
-proc subnetMask*(this: var ETHClass): IPAddress {.cdecl.}
-proc gatewayIP*(this: var ETHClass): IPAddress {.cdecl.}
-proc dnsIP*(this: var ETHClass; dns_no: uint8 = 0): IPAddress {.cdecl.}
-proc broadcastIP*(this: var ETHClass): IPAddress {.cdecl.}
-proc networkID*(this: var ETHClass): IPAddress {.cdecl.}
-proc subnetCIDR*(this: var ETHClass): uint8 {.cdecl.}
-proc macAddress*(this: var ETHClass; mac: ptr uint8): ptr uint8 {.cdecl.}
-proc macAddress*(this: var ETHClass): String {.cdecl.}
-var ETH*: ETHClass
+            dns2: IPAddress = cast[uint32](0x00000000)): bool {.cdecl,
+    importcpp: "config", header: "ETH.h".}
+proc getHostname*(this: var ETHClass): cstring {.cdecl, importcpp: "getHostname",
+    header: "ETH.h".}
+proc setHostname*(this: var ETHClass; hostname: cstring): bool {.cdecl,
+    importcpp: "setHostname", header: "ETH.h".}
+proc fullDuplex*(this: var ETHClass): bool {.cdecl, importcpp: "fullDuplex",
+                                        header: "ETH.h".}
+proc linkUp*(this: var ETHClass): bool {.cdecl, importcpp: "linkUp", header: "ETH.h".}
+proc linkSpeed*(this: var ETHClass): uint8 {.cdecl, importcpp: "linkSpeed",
+    header: "ETH.h".}
+proc enableIpV6*(this: var ETHClass): bool {.cdecl, importcpp: "enableIpV6",
+                                        header: "ETH.h".}
+proc localIPv6*(this: var ETHClass): IPv6Address {.cdecl, importcpp: "localIPv6",
+    header: "ETH.h".}
+proc localIP*(this: var ETHClass): IPAddress {.cdecl, importcpp: "localIP",
+    header: "ETH.h".}
+proc subnetMask*(this: var ETHClass): IPAddress {.cdecl, importcpp: "subnetMask",
+    header: "ETH.h".}
+proc gatewayIP*(this: var ETHClass): IPAddress {.cdecl, importcpp: "gatewayIP",
+    header: "ETH.h".}
+proc dnsIP*(this: var ETHClass; dns_no: uint8 = 0): IPAddress {.cdecl,
+    importcpp: "dnsIP", header: "ETH.h".}
+proc broadcastIP*(this: var ETHClass): IPAddress {.cdecl, importcpp: "broadcastIP",
+    header: "ETH.h".}
+proc networkID*(this: var ETHClass): IPAddress {.cdecl, importcpp: "networkID",
+    header: "ETH.h".}
+proc subnetCIDR*(this: var ETHClass): uint8 {.cdecl, importcpp: "subnetCIDR",
+    header: "ETH.h".}
+proc macAddress*(this: var ETHClass; mac: ptr uint8): ptr uint8 {.cdecl,
+    importcpp: "macAddress", header: "ETH.h".}
+proc macAddress*(this: var ETHClass): String {.cdecl, importcpp: "macAddress",
+    header: "ETH.h".}
+
+var ETH* {.importcpp: "ETH", header: "ETH.h".}: ETHClass
